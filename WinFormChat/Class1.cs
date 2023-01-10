8666ff535d6c8de411ad3f8dd0884d0eda0b7c94
Claude Agnes <rlawnstjddl12@gmail.com>
2023-01-10 14:20:17 +0900

    초기 커밋.

---
+++ WinFormChat/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WinFormChat
+{
+    class Class1
+    {
+        TcpClient clientSocket = null;
+        public Dictionary<TcpClient, string> clientList = null;
+
+        public void startClient(TcpClient clientSocket, Dictionary<TcpClient, string> clientList)
+        {
+            this.clientSocket = clientSocket;
+            this.clientList = clientList;
+
+            Thread t_handler = new Thread(doChat);
+        }
+
+        public delegate void MessageDisplayHandler(string message, string user_name);
+        public event MessageDisplayHandler OnReceived;
+
+        public delegate void DisconnectedHandler(TcpClient clientSocket);
+        public event DisconnectedHandler OnDisconnected;
+
+        private void doChat()
+        {
+            NetworkStream stream = null;
+            try
+            {
+                byte[] buffer = new byte[1024];
+                string msg = string.Empty;
+            }
+        }
+    }
+}
